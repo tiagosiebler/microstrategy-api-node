ee4fc6993e2b7d78298db45a733edd439ae3a6f1
TS <ts@github.com>
2020-04-02 11:14:12 +0100

    start work on reports api

---
+++ samples/reports/reportSample1.js
@@ -0,0 +1,40 @@
+const mstr = require('../../lib/mstr.js');
+
+(async ()=> {
+  const baseUrl = 'http://aps-tsiebler-vm:8080/11.1GALibrary/api';
+  const mstrApi = new mstr.REST({
+    baseUrl: baseUrl
+  });
+
+  await mstrApi.login({
+    username: 'Administrator',
+    password: '',
+    loginMode: 1
+  });
+
+  // MicroStrategy Tutorial
+  const projectId = 'B19DEDCC11D4E0EFC000EB9495D0F44F';
+  mstrApi.setProjectId(projectId);
+
+  const objectId = '0C9AF3144240E7FC3EC6028137A9655C';
+
+  const ReportsAPI = mstrApi.reports;
+
+  try {
+    const defn = await ReportsAPI.getReportDefinition(objectId);
+    console.log('res: ', JSON.stringify(defn, null, 2));
+
+    // const instanceInfo = await ReportsAPI.createReportInstance(objectId);
+    // console.log('res: ', JSON.stringify(instanceInfo, null, 2));
+
+    const instanceId = instanceInfo.instanceId;
+    // const instance = await ReportsAPI.getReportInstance(objectId, instanceId);
+    // console.log('res: ', JSON.stringify(instance, null, 2));
+
+    console.log('EOF');
+  } catch (e) {
+    console.error(e);
+  }
+
+  await mstrApi.logout();
+})();
