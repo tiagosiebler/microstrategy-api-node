58a1125bb5fd165f0738eb7e0230260895262f86
Enrique Pastrana <enrique.pastrana@gmail.com>
2020-04-24 15:56:03 +0200

    Starting Library wrapper

---
+++ samples/library/manageLibrary.js
@@ -0,0 +1,23 @@
+const mstr = require("../../lib/mstr.js");
+
+(async () => {
+  const baseUrl = "http://10.27.72.72:8080/2020u1Library/api";
+  const mstrApi = new mstr.REST({
+    baseUrl: baseUrl,
+  });
+
+  await mstrApi.login({
+    username: "Administrator",
+    password: "",
+    loginMode: 1,
+  });
+
+  // MicroStrategy Tutorial
+  const projectId = "B19DEDCC11D4E0EFC000EB9495D0F44F";
+  mstrApi.setProjectId(projectId);
+  const fields = "name,id";
+  //const outputFlag = "DEFAULT";
+  const libraryAPI = mstrApi.library;
+  const libraryResult = await libraryAPI.getLibrary(fields);
+  console.log(JSON.stringify(libraryResult, null, 2));
+})();
