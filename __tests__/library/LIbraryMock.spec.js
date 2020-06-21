e2a30405b09a227bb8d978276b2daa510525691e
jestemrique <enrique.pastrana@gmail.com>
2020-06-21 10:00:12 +0200

    Testing library

--- __tests__/library/LIbraryMock.spec.js
+++ __tests__/library/LIbraryMock.spec.js
@@ -1,43 +1,23 @@
-const RestUtil = require('../../lib/util/RestUtil');
 const RestTopic = require('../../lib/restAPIs/library');
+const RestUtil = require('../../lib/util/RestUtil');
+const mockedAxios = require('axios');
 
 jest.mock('../../lib/util/RestUtil');
-//jest.mock('../../lib/util/buildCommonQueryParams');
-
-beforeEach(() => {
-  RestTopic.mockClear();
-});
+jest.mock('axios');
 
 describe('Library endpoints (RestTopic = Library', () => {
-  it('has been called once', () => {
+  test('library.getProjectHeader has been called', async () => {
     const library = new RestTopic();
-    expect(RestTopic).toHaveBeenCalledTimes(1);
+    const result = library.getObject();
+    await expect(library.getProjectHeader).toHaveBeenCalled();
   });
 
-  it('returns valid json object', () => {
-    const library = new RestTopic();
-    const projectId = 'B19DEDCC11D4E0EFC000EB9495D0F44F';
-    const objectId = 'C866FC0B417F8BD4DBBC07BE57C83413';
-    const response = {
-      id: 'C866FC0B417F8BD4DBBC07BE57C83413',
-      recipients: [
-        {
-          description: '',
-          name: 'Administrator',
-          subtype: 8704,
-          id: '54F3D26011D2896560009A8E67019608',
-        },
-      ],
-    };
+  // test('Axios request should have been called', async () => {
+  //   const library = new RestTopic();
 
-    const mocked_makeRequest = jest.fn();
-    RestUtil.prototype._makeRequest = mocked_makeRequest;
-    //mocked_makeRequest.mockReturnValue(Promise.resolve(response));
-
-    //expect(library.getObject(objectId, projectId)).toBe(response);
-    //return expect(mocked_makeRequest).toBe(response);
-    library.getObject(objectId, projectId).then((data) => {
-      expect(data).toBe(response);
-    });
-  });
+  //   mockedAxios.get.mockImplementationOnce(() => Promise.resolve('hola'));
+  //   console.log('siete');
+  //   await expect(mockedAxios.get).toHaveBeenCalled();
+  //   console.log('ocho');
+  // });
 });
