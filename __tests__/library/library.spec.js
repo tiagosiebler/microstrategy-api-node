08175d095013c50aab359645fd7babc787e2fa6d
jestemrique <enrique.pastrana@gmail.com>
2020-06-23 16:09:56 +0200

    Revert "Testing library endpoints"
    This reverts commit 8cebfc563ba7cbb284c0bd13853fdb8c902b6bf8.

--- __tests__/library/library.spec.js
+++ __tests__/library/library.spec.js
@@ -1,36 +1,32 @@
 const RestTopic = require('../../lib/restAPIs/library');
-const RestUtil = require('../../lib/util/RestUtil');
+//jest.mock('../../lib/restAPIs/library');
 
-jest.mock('../../lib/util/RestUtil');
-
-beforeEach(() => {
-  jest.clearAllMocks();
-});
-
-describe('Library endpoints (RestTopic = Library', () => {
-  // it.skip('has been called once', () => {
-  //   const library = new RestTopic(axios);
-  //   expect(axios).toHaveBeenCalled();
-  // });
-  it.skip('RestUtil getProject mock have been called', () => {
-    const library = new RestTopic();
-    const projectId = 'B19DEDCC11D4E0EFC000EB9495D0F44F';
-    const objectId = 'C866FC0B417F8BD4DBBC07BE57C83413';
-    const result = library.getObject(objectId, projectId);
-    expect(library.getProjectHeader).toHaveBeenCalled();
+describe.skip('Library API tests', () => {
+  describe('_getBase()', () => {
+    test('it should return library as topic', () => {
+      const library = new RestTopic();
+      const output = 'library';
+      expect(library._getBase()).toEqual(output);
+    });
   });
+  describe('getObject()', () => {
+    test('it should return valid JSON for object', () => {
+      const library = new RestTopic();
+      const projectId = 'B19DEDCC11D4E0EFC000EB9495D0F44F';
+      const objectId = 'C866FC0B417F8BD4DBBC07BE57C83413';
+      const response = {
+        id: 'C866FC0B417F8BD4DBBC07BE57C83413',
+        recipients: [
+          {
+            description: '',
+            name: 'Administrator',
+            subtype: 8704,
+            id: '54F3D26011D2896560009A8E67019608',
+          },
+        ],
+      };
 
-  // it('RestUtil getProject mock should be Hola', () => {
-  //   const library = new RestTopic();
-  //   const projectId = 'B19DEDCC11D4E0EFC000EB9495D0F44F';
-  //   console.log(.getProjectHeader(projectId));
-  //   expect(library.getProjectHeader(projectId)).toEqual('Hola');
-  // });
-  // it('RestUtil _makeRequest mock have been called', async () => {
-  //   const library = new RestTopic();
-  //   const projectId = 'B19DEDCC11D4E0EFC000EB9495D0F44F';
-  //   const objectId = 'C866FC0B417F8BD4DBBC07BE57C83413';
-  //   const result = await library.getObject(objectId, projectId);
-  //   expect(library._makeRequest).toHaveBeenCalled();
-  // });
+      expect(library.getObject(objectId, projectId)).toBe(response);
+    });
+  });
 });
