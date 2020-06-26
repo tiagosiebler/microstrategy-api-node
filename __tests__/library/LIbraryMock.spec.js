cad316f8729d6d33f74359c12199633260df2ba6
jestemrique <enrique.pastrana@gmail.com>
2020-06-26 15:41:46 +0200

    library tests

--- __tests__/library/LIbraryMock.spec.js
+++ __tests__/library/LIbraryMock.spec.js
@@ -11,20 +11,25 @@ describe('Testing Library API', () => {
     const objectId = 'C866FC0B417F8BD4DBBC07BE57C83413';
     const baseUrl = 'http://10.23.1.124:8080/MicroStrategyLibrary/api';
     const mstrApi = new mstr.REST({ baseUrl: baseUrl });
+    mstrApi.setProjectId(projectId);
     expect(axios.request).toHaveBeenCalled();
-    expect(axios.request).toHaveBeenCalledWith({ method: 'get' });
-    const libraryApi = mstrApi.library;
-    const object = library.getObject();
-    expect(axios.request).toHaveBeenCalledWith({ method: 'get' });
   });
 
-  it(' should have called axios with GET', async () => {
+  it(' should have called axios in login', async () => {
     const projectId = 'B19DEDCC11D4E0EFC000EB9495D0F44F';
     const objectId = 'C866FC0B417F8BD4DBBC07BE57C83413';
     const baseUrl = 'http://10.23.1.124:8080/MicroStrategyLibrary/api';
     const mstrApi = new mstr.REST({ baseUrl: baseUrl });
-    const libraryApi = mstrApi.library;
-    const object = library.getObject();
-    expect(axios.request).toHaveBeenCalledWith({ method: 'get' });
+    axios.post.mockResolvedValue('hola');
+    await mstrApi.login({
+      username: 'Administrator',
+      password: '',
+      loginMode: 1,
+    });
+    expect(axios.post).toHaveBeenCalled();
+    // mstrApi.setProjectId(projectId);
+    // const library = mstrApi.library;
+    // const object = library.getObject();
+    // expect(axios.request).toHaveBeenCalledWith({ method: 'get' });
   });
 });
