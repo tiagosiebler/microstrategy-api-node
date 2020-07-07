27ab1ace55eddcfe626693a9b631bb90c8bd04dd
tiagosiebler <tiagosiebler@github.com>
2020-07-07 14:07:22 +0100

    add prod and dev bundles

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,30 +1,39 @@
 const path = require('path');
 
-module.exports = {
-  entry: './lib/mstr.js',
-  output: {
-    filename: 'mstrapi.min.js',
-    path: path.resolve(__dirname, '../dist'),
-    library: 'mstrapi',
-    libraryTarget: 'var'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components|samples)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env', {
-              'targets': {
-                'node': 'current'
-              }
-            }]]
+module.exports = (env = '') => {
+  console.log('env: ', env);
+  const isDev = env && env == 'dev';
+  const fileName = isDev ? 'mstrapi.dev' : 'mstrapi';
+
+  return {
+    entry: './lib/mstr.js',
+    output: {
+      filename: `${fileName}.min.js`,
+      path: path.resolve(__dirname, '../dist'),
+      library: 'mstrapi',
+      libraryTarget: 'var'
+    },
+    devtool: 'source-map',
+    plugins: [
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components|samples|dist)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [['@babel/preset-env', {
+                'targets': {
+                  'node': 'current'
+                }
+              }]]
+            }
           }
         }
-      }
-    ]
-  },
-  mode: 'production'
+      ]
+    },
+    mode: isDev ? 'development' : 'production'
+  };
 };
