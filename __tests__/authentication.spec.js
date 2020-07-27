8d4911015314b35332cb501cd97647e1678914b2
Jestemrique <enrique.pastrana@gmail.com>
2020-07-27 16:12:08 +0200

    Adding tests for authentication category

--- __tests__/authentication.spec.js
+++ __tests__/authentication.spec.js
@@ -26,7 +26,7 @@ describe('MSTR REST', () => {
     loginMode: 1,
   };
 
-  describe('Authentication', () => {
+  describe('Authentication -> Login', () => {
     it('axios should have been called', async () => {
       const mstrApi = new mstr.REST({
         baseUrl: testUrl,
@@ -36,7 +36,7 @@ describe('MSTR REST', () => {
       expect(axios).toHaveBeenCalled();
     });
 
-    it('should store auth token after successs ful lllogin', async () => {
+    it('should store auth token after successsful login', async () => {
       const mstrApi = new mstr.REST({
         baseUrl: testUrl,
       });
@@ -64,6 +64,77 @@ describe('MSTR REST', () => {
           Accept: 'application/json',
           'Accept-Encoding': 'gzip, deflate, br',
           'Content-Type': 'application/json',
+        },
+      });
+      expect(axios).toHaveBeenCalledWith(headers);
+    });
+  });
+
+  //POST /api/logout
+  describe('Authentication-> logout', () => {
+    it('Axios should have been called', async () => {
+      const mstrApi = new mstr.REST({
+        baseUrl: testUrl,
+      });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      expect(axios).toHaveBeenCalledTimes(1);
+      jest.clearAllMocks();
+      mstrApi.logout();
+      expect(axios).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should have session information removed', async () => {
+      const mstrApi = new mstr.REST({
+        baseUrl: testUrl,
+      });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      const headers = mstrApi.getSessionHeaders();
+
+      mstrApi.logout();
+      expect(axios).toHaveBeenCalledTimes(2);
+      //TODO
+      //Test having X-MSTR-AuthToken set to undefined.
+    });
+
+    it('should have been called with the correct endpoint', async () => {
+      const mstrApi = new mstr.REST({
+        baseUrl: testUrl,
+      });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      jest.clearAllMocks();
+      mstrApi.logout();
+      expect(axios).toHaveBeenCalledTimes(1);
+      const endpoint = expect.stringMatching('api/auth/logout');
+      expect(axios).toHaveBeenCalledWith(
+        expect.objectContaining({ url: endpoint })
+      );
+    });
+
+    it('Should have ben caleed with POST method', async () => {
+      const mstrApi = new mstr.REST({
+        baseUrl: testUrl,
+      });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      jest.clearAllMocks();
+      mstrApi.logout();
+      expect(axios).toHaveBeenCalledTimes(1);
+      const method = expect.objectContaining({ method: 'POST' });
+      expect(axios).toHaveBeenCalledWith(method);
+    });
+
+    it('Should have been called with correct headers', async () => {
+      const mstrApi = new mstr.REST({
+        baseUrl: testUrl,
+      });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      jest.clearAllMocks();
+      mstrApi.logout();
+      const headers = expect.objectContaining({
+        headers: {
+          Accept: 'application/json',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Content-Type': 'application/json',
+          'X-MSTR-AuthToken': 'mockAuthToken',
         },
       });
       expect(axios).toHaveBeenCalledWith(headers);
