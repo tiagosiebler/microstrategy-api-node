26e714176bcda8a15338895b9d1f619fef1b39b9
Jestemrique <enrique.pastrana@gmail.com>
2020-07-28 16:50:13 +0200

    adding tests to authentication

--- __tests__/authentication.spec.js
+++ __tests__/authentication.spec.js
@@ -1,17 +1,48 @@
 const mstr = require('../lib/mstr');
 const axios = require('axios');
 
-jest.mock('axios', () =>
-  jest.fn(() =>
-    Promise.resolve({
-      status: 204,
-      headers: { 'x-mstr-authtoken': 'mockAuthToken' },
-      //headers: {
+// jest.mock('axios', () =>
+//   jest.fn(() =>
+//     Promise.resolve({
+//       status: 204,
+//       headers: { 'x-mstr-authtoken': 'mockAuthToken' },
+//     })
+//   )
+// );
 
-      //}
-    })
-  )
+/********************************/
+
+jest.mock('axios', () =>
+  jest.fn((axios) => {
+    const endpoint = axios.url.substring(axios.url.indexOf('api'));
+
+    switch (endpoint) {
+      case 'api/auth/identityToken?':
+        return Promise.resolve({
+          status: 201,
+          headers: {
+            //'x-mstr-authtoken': 'mockAuthToken',
+            'x-mstr-identitytoken': 'mockIdentityToken',
+          },
+        });
+        break;
+      case 'api/auth/login?':
+        return Promise.resolve({
+          status: 204,
+          headers: { 'x-mstr-authtoken': 'mockAuthToken' },
+        });
+        break;
+      default:
+        return Promise.resolve({
+          status: 204,
+          headers: { 'x-mstr-authtoken': 'mockAuthToken' },
+        });
+        break;
+    }
+  })
 );
+
+/********************************/
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -89,11 +120,13 @@ describe('MSTR REST', () => {
       });
       const sessionInfo = await mstrApi.login(loginInfo);
       const headers = mstrApi.getSessionHeaders();
-
-      mstrApi.logout();
+      const authToken = headers['X-MSTR-AuthToken'];
+      mstrApi.logout(authToken);
       expect(axios).toHaveBeenCalledTimes(2);
+      /************************************************/
       //TODO
       //Test having X-MSTR-AuthToken set to undefined.
+      /************************************************/
     });
 
     it('should have been called with the correct endpoint', async () => {
@@ -138,6 +171,111 @@ describe('MSTR REST', () => {
         },
       });
       expect(axios).toHaveBeenCalledWith(headers);
+    });
+  });
+
+  //POST /api/auth/identityToken
+  describe('Authentication -> createIdentityToken', () => {
+    it('axios should have been called', async () => {
+      const mstrApi = new mstr.REST({
+        baseUrl: testUrl,
+      });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      const sessionHeaders = mstrApi.getSessionHeaders();
+      const authToken = sessionHeaders['X-MSTR-AuthToken'];
+      expect(axios).toHaveBeenCalledTimes(1);
+      jest.clearAllMocks();
+      mstrApi.authentication.createIdentityToken(authToken);
+      expect(axios).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should have been called with correct endpoint', async () => {
+      const mstrApi = new mstr.REST({ baseUrl: testUrl });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      const endPoint = expect.stringMatching('api/auth/identityToken');
+      const sessionHeaders = mstrApi.getSessionHeaders();
+      const authToken = sessionHeaders['X-MSTR-AuthToken'];
+      const identityToken = await mstrApi.authentication.createIdentityToken(
+        authToken
+      );
+      expect(axios).toHaveBeenCalledWith(
+        expect.objectContaining({ url: endPoint })
+      );
+    });
+
+    it('Should have been caleed with correct headers', async () => {
+      const mstrApi = new mstr.REST({ baseUrl: testUrl });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      const sessionHeaders = mstrApi.getSessionHeaders();
+      const authToken = sessionHeaders['X-MSTR-AuthToken'];
+      const headers = expect.objectContaining({
+        headers: {
+          Accept: 'application/json',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Content-Type': 'application/json',
+        },
+      });
+      const identityToken = mstrApi.authentication.createIdentityToken(
+        authToken
+      );
+      expect(axios).toHaveBeenCalledWith(headers);
+    });
+    //TODO
+    it('Should have been called with POST method', async () => {
+      const mstrApi = new mstr.REST({ baseUrl: testUrl });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      expect(axios).toHaveBeenCalledTimes(1);
+      jest.clearAllMocks();
+      const method = expect.objectContaining({ method: 'POST' });
+      const sessionHeaders = mstrApi.getSessionHeaders();
+      const authToken = sessionHeaders['X-MSTR-AuthToken'];
+      const identityToken = await mstrApi.authentication.createIdentityToken(
+        authToken
+      );
+      expect(axios).toHaveBeenCalledWith(method);
+    });
+  });
+
+  //GET /api/auth/identityToken
+  describe('Authentication -> validateIdentityToken', () => {
+    it('Axios should have been called', async () => {
+      const mstrApi = new mstr.REST({ baseUrl: testUrl });
+      const sessionInfo = mstrApi.login(loginInfo);
+      expect(axios).toHaveBeenCalledTimes(1);
+    });
+    it('Should have been called with correct endpoint', async () => {
+      const mstrApi = new mstr.REST({ baseUrl: testUrl });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      expect(axios).toHaveBeenCalledTimes(1);
+      const endPoint = expect.stringMatching('api/auth/identityToken');
+      const sessionHeaders = mstrApi.getSessionHeaders();
+      const authToken = sessionHeaders['X-MSTR-AuthToken'];
+      const identityToken = await mstrApi.authentication.createIdentityToken(
+        authToken
+      );
+      expect(axios).toHaveBeenCalledWith(
+        expect.objectContaining({ url: endPoint })
+      );
+    });
+    it('should have been called with correct headers', async () => {
+      const mstrApi = new mstr.REST({ baseUrl: testUrl });
+      const sessionInfo = await mstrApi.login(loginInfo);
+      expect(axios).toHaveBeenCalledTimes(1);
+      const sessionHeaders = mstrApi.getSessionHeaders();
+      const authToken = sessionHeaders['X-MSTR-AuthToken'];
+      // const identityToken = await mstrApi.authentication.createIdentityToken(
+      //   authToken
+      // );
+      const identity = await mstrApi.authentication.createIdentityToken(
+        authToken
+      );
+      const identityToken = sessionHeaders['-MSTR-identityToken'];
+    });
+
+    it('Should have been called with GET method', async () => {
+      const mstrApi = new mstr.REST({ baseUrl: testUrl });
+      const sessionInfo = mstrApi.login(loginInfo);
+      expect(axios).toHaveBeenCalledTimes(1);
     });
   });
 });
